b236132aa83283e663ec4a33818b7d182c350eeb
Simon Hegele <103111576+SimonHegele@users.noreply.github.com>
2025-03-27 12:45:56 +0100

    Update utrpy_io.py

--- src/utrpy/utrpy_io.py
+++ src/utrpy/utrpy_io.py
@@ -2,17 +2,20 @@
 Functions for file reading and writing
 """
 
-from pandas import read_csv
+from logging import info
+from pandas  import read_csv
 
 def load_data(args):
 
+    info("Reading ...")
     gff_prediction = read_csv(args.gff_prediction, sep="\t", header=None, comment="#")
     gff_assembly   = read_csv(args.gff_assembly,   sep="\t", header=None, comment="#")
 
     return gff_prediction, gff_assembly
 
 def write_data(args, gffs, scaffolds):
-    
+
+    info("Writing ...")
     gff_utrpy = sorted(gffs, key=lambda x: scaffolds.index(x.iloc[0][0]))
     for df in gff_utrpy:
         df.to_csv(args.gff_utrpy, sep="\t", mode="a", index=False, header=None)
