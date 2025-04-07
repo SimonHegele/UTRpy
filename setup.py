865f1fa232b9bb64ecabd92a115d101df23ffa59
Simon Hegele <103111576+SimonHegele@users.noreply.github.com>
2025-04-07 18:23:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="UTRpy",
-    version="0.1",
+    version="0.2",
     license="GPL3",
     description="UTR extensions for annotations from protein orthology based gene prediction tools using exons from reference based transcriptome assembly",
     package_dir={"": "src"},
